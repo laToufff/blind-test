c1d260031aae8b04cb6d80cb2ab1c629f6846661
Liam Kern <liam.kern@protonmail.com>
2024-07-21 01:50:31 +0200

    stream random part of a random song

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
+const mm = require('music-metadata');
 
 
 const app = express();
@@ -11,7 +12,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/play', (req, res) => {
-    const filePath = path.join(__dirname + '/../songs/03 - Time Is Running Out.mp3');
+    const fileList = fs.readdirSync(path.join(__dirname + '/../songs'));
+    const songList = fileList.filter(file => file.endsWith('.mp3'));
+    const randi = Math.floor(Math.random() * songList.length);
+
+    const filePath = path.join(__dirname + '/../songs/'+songList[randi]);
 
     fs.stat(filePath, (err, stats) => {
         if (err) {
@@ -20,34 +25,56 @@ app.get('/play', (req, res) => {
             res.end('File not found');
             return;
         }
+        mm.parseFile(filePath).then(metadata => {
+            //console.log(metadata.format);
+            const bitrate = metadata.format.bitrate;
+            const length = metadata.format.duration;
 
-        const range = req.headers.range;
-
-        if (range) {
+            const bytesPerSecond = bitrate / 8;
             const fileSize = stats.size;
-            const chunkSize = 1024 * 1024;
-            const start = Number(range.replace(/\D/g, ""));
-            const end = Math.min(start + chunkSize, fileSize - 1);
-
-            res.writeHead(206, {
-                'Content-Type': 'audio/mpeg',
-                'Content-Length': end - start,
-                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-                'Accept-Ranges': 'bytes',
-            });
-
-            const stream = fs.createReadStream(filePath, { start, end });
+            const duration = 10;
+            const startSec = Math.floor(Math.random() * (length - duration));
+            console.log('startSec', startSec);
+            const startByte = startSec * bytesPerSecond;
+            const endByte = startByte + duration * bytesPerSecond;
+            if (endByte > fileSize) {
+                endByte = fileSize;
+            }
             
-            stream.pipe(res);
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'audio/mpeg',
-                'Content-Length': stats.size,
-            });
-
-            const stream = fs.createReadStream(filePath);
-            stream.pipe(res);
-        }
+            /*const range = req.headers.range;
+            if (range) {
+                const chunkSize = 1024 * 1024;
+                const startRange = Number(range.replace(/\D/g, ""));
+                const start = startByte + startRange;
+                const end = Math.min(start + chunkSize, endByte);
+                console.log('start', start);
+                console.log('end', end);
+    
+                res.writeHead(206, {
+                    'Content-Type': 'audio/mpeg',
+                    'Content-Length': end - start,
+                    'Content-Range': `bytes ${start}-${end}/${duration * bytesPerSecond}`,
+                    'Accept-Ranges': 'bytes',
+                });
+    
+                const stream = fs.createReadStream(filePath, { start, end });
+                
+                stream.pipe(res);
+            
+            } else {*/
+                res.writeHead(200, {
+                    'Content-Type': 'audio/mpeg',
+                    'Content-Length': duration * bytesPerSecond,
+                    'Content-Range': `bytes ${startByte}-${endByte}/${duration * bytesPerSecond}`,
+                    'Accept-Ranges': 'bytes',
+                    'Cache-Control': 'no-store'
+                });
+    
+                const stream = fs.createReadStream(filePath, { start: startByte, end: endByte });
+                stream.pipe(res);
+            //}
+        });
+        
     });
 });
 
